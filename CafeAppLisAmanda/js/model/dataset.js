10a3f9ff72d4a83f7bd7827eba26e03dad5c3d80
AmandaBonfim <91920245+AmandaBonfim@users.noreply.github.com>
2022-03-01 21:00:09 -0300

    Add files via upload

---
+++ CafeAppLisAmanda/js/model/dataset.js
@@ -0,0 +1,36 @@
+const dataset = [
+  {
+    id: 1,
+    name: 'Sonho',
+    image: 'imagem/sonho.jpg',
+    description:'O sonho é muito comum em padarias. Feito de ovos e farinha de trigo, com um delicioso creme de confeteiro.',
+    price: 'R$10.00',
+  },
+  {
+    id: 2,
+    name: 'Sanduíche',
+    image: 'imagem/sanduiche.jpg',
+    description:
+      'O sanduíche , é um tipo de alimento que consiste em duas fatias de um pão inteiro, entre as quais é colocada carne, queijo ou outro tipo de alimento.Os sanduíches são, habitualmente, consumidos ao lanche ou como uma refeição rápida, durante o almoço ou o jantar.',
+    price: 'R$17.00',
+  },
+  {
+    id: 3,
+    name: 'Café',
+    image: 'imagem/cafe.jpg',
+    description:
+      'O café é uma bebida produzida a partir dos grãos torrados do fruto do cafeeiro. É servido tradicionalmente quente, mas também pode ser consumido gelado. O café é um estimulante, por possuir cafeína — geralmente 80 a 140 mg para cada 207 ml dependendo do método de preparação.',
+    price: 'R$7.00',
+  },
+  {
+    id: 4,
+    name: 'Suco de Laranja',
+    image: 'imagem/suco.jpg',
+    description:
+      'O suco é uma bebida produzida do líquido extraído de frutos.Muito  refrescante para dias de calor.',
+    price: 'R$10.00',
+  },
+    
+];
+
+export default dataset;
